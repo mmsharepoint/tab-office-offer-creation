2294d5bd3b56f073ce489468012fb4d99b098a48
mmsharepoint <markus.moeller.ed@outlook.de>
2022-11-17 09:48:45 +0100

    Initial templates

---
+++ templates/deploy.ps1
@@ -0,0 +1,10 @@
+
+Connect-PnPOnline -Url $siteUrl -Interactive
+
+Set-PnPSite -NoScriptSite $false
+Invoke-PnPSiteTemplate -Path ".\templates\Offerings.xml"
+
+$ct = Get-PnPContentType -Identity "Offering"
+Set-PnPContentType -Identity $ct -UpdateChildren
+
+Set-PnPSite -NoScriptSite $true
